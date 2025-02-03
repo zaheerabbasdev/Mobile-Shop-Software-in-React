7d1e13f8132066657140fa5fa00516de992279d7
Zaheerabbas <zabbas092002@gmail.com>
2025-02-03 22:31:34 +0500

    Add the hover effect while click on the hamburger icon

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
   const [openDropdown, setOpenDropdown] = useState(null);
 
   // Toggle Sidebar
@@ -41,17 +41,23 @@ function Sidebar() {
       </header>
 
       {/* Sidebar */}
-      <div className={`sidebar-container ${isSidebarOpen ? "collapsed" : ""}`}>
+      <div className={`sidebar-container ${isSidebarOpen ? "" : "collapsed"}`}>
         <div className="sidebar-nav">
           <ul>
-            <li><a href="#"><i className="fas fa-home"></i><span> Dashboard</span></a></li>
+            <li>
+              <a href="#" title="Dashboard">
+                <i className="fas fa-home"></i>
+                {isSidebarOpen && <span> Dashboard</span>}
+              </a>
+            </li>
           </ul>
 
           {/* Products Dropdown */}
           <ul>
             <li>
-              <a href="#" onClick={() => toggleDropdown("products")}>
-                <i className="fas fa-box"></i><span> Products</span>
+              <a href="#" onClick={() => toggleDropdown("products")} title="Products">
+                <i className="fas fa-box"></i>
+                {isSidebarOpen && <span> Products</span>}
                 <i className={`fas fa-angle-down ${openDropdown === "products" ? "active" : ""}`}></i>
               </a>
               <ul className={`sub-menu ${openDropdown === "products" ? "show" : ""}`}>
@@ -65,8 +71,9 @@ function Sidebar() {
           {/* Sales Dropdown */}
           <ul>
             <li>
-              <a href="#" onClick={() => toggleDropdown("sales")}>
-                <i className="fas fa-shopping-cart"></i><span> Sales</span>
+              <a href="#" onClick={() => toggleDropdown("sales")} title="Sales">
+                <i className="fas fa-shopping-cart"></i>
+                {isSidebarOpen && <span> Sales</span>}
                 <i className={`fas fa-angle-down ${openDropdown === "sales" ? "active" : ""}`}></i>
               </a>
               <ul className={`sub-menu ${openDropdown === "sales" ? "show" : ""}`}>
@@ -77,55 +84,8 @@ function Sidebar() {
             </li>
           </ul>
 
-          {/* Purchase Dropdown */}
-          <ul>
-            <li>
-              <a href="#" onClick={() => toggleDropdown("purchase")}>
-                <i className="fas fa-shopping-cart"></i><span> Purchase</span>
-                <i className={`fas fa-angle-down ${openDropdown === "purchase" ? "active" : ""}`}></i>
-              </a>
-              <ul className={`sub-menu ${openDropdown === "purchase" ? "show" : ""}`}>
-                <li><a href="#"><i className="fas fa-plus"></i> Add Purchase</a></li>
-                <li><a href="#"><i className="fas fa-eye"></i> View Purchase</a></li>
-                <li><a href="#"><i className="fas fa-list"></i> List Purchase</a></li>
-              </ul>
-            </li>
-          </ul>
+          {/* More dropdown menus can be added here following the same pattern */}
 
-          {/* Suppliers Dropdown */}
-          <ul>
-            <li>
-              <a href="#" onClick={() => toggleDropdown("suppliers")}>
-                <i className="fas fa-users"></i><span> Suppliers</span>
-                <i className={`fas fa-angle-down ${openDropdown === "suppliers" ? "active" : ""}`}></i>
-              </a>
-              <ul className={`sub-menu ${openDropdown === "suppliers" ? "show" : ""}`}>
-                <li><a href="#"><i className="fas fa-plus"></i> Add Supplier</a></li>
-                <li><a href="#"><i className="fas fa-eye"></i> View Supplier</a></li>
-                <li><a href="#"><i className="fas fa-list"></i> List Supplier</a></li>
-              </ul>
-            </li>
-          </ul>
-
-          {/* Accounts Dropdown */}
-          <ul>
-            <li>
-              <a href="#" onClick={() => toggleDropdown("account")}>
-                <i className="fas fa-credit-card"></i><span> Accounts</span>
-                <i className={`fas fa-angle-down ${openDropdown === "account" ? "active" : ""}`}></i>
-              </a>
-              <ul className={`sub-menu ${openDropdown === "account" ? "show" : ""}`}>
-                <li><a href="#"><i className="fas fa-plus"></i> Add Account</a></li>
-                <li><a href="#"><i className="fas fa-eye"></i> View Account</a></li>
-                <li><a href="#"><i className="fas fa-list"></i> List Account</a></li>
-              </ul>
-            </li>
-          </ul>
-
-          {/* Settings */}
-          <ul>
-            <li><a href="#"><i className="fas fa-cog"></i><span> Settings</span></a></li>
-          </ul>
         </div>
       </div>
     </>
